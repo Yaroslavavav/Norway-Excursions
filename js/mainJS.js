9f9ff98b13381929bbe6689a69a74faf9962ce28
Yaroslavavav <yaroslavavav96@gmail.com>
2019-06-18 15:05:02 +0300

    mobile version added

--- js/mainJS.js
+++ js/mainJS.js
@@ -2,7 +2,7 @@
     //initialize swiper when document ready
     var swiper = new Swiper('.swiper-container', {
       slidesPerView: 4,
-      spaceBetween: 35,
+      spaceBetween: 20,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -11,6 +11,27 @@
         nextEl: '.swiper-button-nex',
         prevEl: '.swiper-button-pre ',
       },
+    breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is <= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is <= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1240: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
     });
     var swiperReviews = new Swiper('.swiper-container-reviews', {
       slidesPerView: 4,
